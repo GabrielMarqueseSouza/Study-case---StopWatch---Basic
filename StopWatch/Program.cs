ff70f480ce8baee6f8ff1fcbd07ce7ce1fbe325a
Gabriel Marques <gabrielmarques.businessemail@gmail.com>
2022-03-17 17:22:06 -0300

    Implemented Start method

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace StopWatch
 {
@@ -6,7 +7,30 @@ namespace StopWatch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the time you want to count:");
+            int enteredTime = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Starting count:");
+            Thread.Sleep(1500);
+
+            Start(enteredTime);
+        }
+
+        static void Start(int time)
+        {
+            int counter = 0;
+
+            while (counter != time)
+            {
+                Console.Clear();
+                counter++;
+                Console.WriteLine(counter);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Count finished!");
+            Thread.Sleep(2000);
         }
     }
 }
