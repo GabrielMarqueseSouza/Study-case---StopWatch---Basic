28098d57a02bb3c43d549f2e5898ce4a951f5cc1
Gabriel Marques <gabrielmarques.businessemail@gmail.com>
2022-03-18 13:01:01 -0300

    StopWatch method extracted to own class and referenced in program class

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-using System.Threading;
 
 namespace StopWatch
 {
     public class Program
     {
-        static void Main(string[] args)
+        readonly StopWatchProgram stopWatch = new();
+        public void Main(string[] args)
         {
+            
             Console.WriteLine("Multitask Program");
             Console.WriteLine("Please, choose what program wich you want to use:");
             Console.WriteLine("1 = StopWatch;");
@@ -17,53 +18,11 @@ namespace StopWatch
             switch (userOption)
             {
                 case 1:
-                    StopWatchMenu();
+                    stopWatch.StopWatchMenu();
                     break;
             }
         }
 
-        static void StopWatchMenu()
-        {
-            Console.Clear();
-            Console.WriteLine("Welcome to stopwatch!");
-            Console.WriteLine("S/s = Seconds");
-            Console.WriteLine("M/m = Minutes");
-            Console.WriteLine("0 = Exit stopwatch");
-            Console.Write("Enter time and counting format (ex: 10s / 1m / 50s): ");
-            string data = Console.ReadLine().ToLower();
-
-            char type = char.Parse(data.Substring(data.Length - 1, 1));
-            int time = int.Parse(data.Substring(0, data.Length - 1));
-            int multiplier = 1;
-
-            if (type == 'm' || type == 'M')
-                multiplier = 60;
-
-            if (time == 0)
-                System.Environment.Exit(0);
-
-            Console.WriteLine("Starting count:");
-            Start(time * multiplier);
-        }
-
-        static void Start(int time)
-        {
-            Thread.Sleep(1000);
-            int counter = 0;
-
-            while (counter != time)
-            {
-                Console.Clear();
-                
-                counter++;
-                Console.WriteLine(counter);
-                Thread.Sleep(1000);
-            };
-
-            Console.Clear();
-            Console.WriteLine("Count finished!");
-            Thread.Sleep(1500);
-            StopWatchMenu();
-        }
+       
     }
 }
---
+++ StopWatch/StopWatchProgram.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+namespace StopWatch
+{
+    public class StopWatchProgram
+    {
+        public void StopWatchMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to stopwatch!");
+            Console.WriteLine("S/s = Seconds");
+            Console.WriteLine("M/m = Minutes");
+            Console.WriteLine("0 = Exit stopwatch");
+            Console.Write("Enter time and counting format (ex: 10s / 1m / 50s): ");
+            string data = Console.ReadLine().ToLower();
+
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm' || type == 'M')
+                multiplier = 60;
+
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            Console.WriteLine("Starting count:");
+            Start(time * multiplier);
+        }
+
+        public void Start(int time)
+        {
+            Thread.Sleep(1000);
+            int counter = 0;
+
+            while (counter != time)
+            {
+                Console.Clear();
+
+                counter++;
+                Console.WriteLine(counter);
+                Thread.Sleep(1000);
+            };
+
+            Console.Clear();
+            Console.WriteLine("Count finished!");
+            Thread.Sleep(1500);
+            StopWatchMenu();
+        }
+    }
+}
